631e9f8d7095b1e0136e7ada535f2a807a29db3a
Ahmed Ibrahim <ahmed.ibrahim@efreshli.com>
2024-10-13 12:12:10 +0300

    add subclasses implementatios, used factory method, overrided all methods

--- Sources/Parrot/Parrot.swift
+++ Sources/Parrot/Parrot.swift
@@ -2,9 +2,9 @@ import Foundation
 
 class Parrot {
     private let type: ParrotType
-    private let numberOfCoconuts: Int
-    private let voltage: Double
-    private let isNailed: Bool
+    let numberOfCoconuts: Int
+    let voltage: Double
+    let isNailed: Bool
 
     init(_ type: ParrotType, _ numberOfCoconuts: Int, _ voltage: Double, _ isNailed: Bool) {
         self.type = type
@@ -13,88 +13,74 @@ class Parrot {
         self.isNailed = isNailed
     }
 
-    func speed() -> Double {
-        switch type {
-        case .european:
-            return baseSpeed
+    func speed() -> Double { 0.0 }
 
-        case .african:
-            return max(0, baseSpeed - loadFactor * Double(numberOfCoconuts))
-
-        case .norwegianBlue:
-            return isNailed ? 0 : baseSpeed(voltage: voltage)
-        }
-    }
-
-    private func baseSpeed(voltage: Double) -> Double {
+    func baseSpeed(voltage: Double) -> Double {
         min(24.0, voltage * baseSpeed)
     }
 
-    private var loadFactor: Double {
+    var loadFactor: Double {
         9.0
     }
 
-    private var baseSpeed: Double {
+    var baseSpeed: Double {
         12.0
     }
 
-    func getCry() -> String {
-        var value: String
-        switch type {
-        case .european:
-            value = "Sqoork!"
-        case .african:
-            value = "Sqaark!"
-        case .norwegianBlue:
-            value = voltage > 0 ? "Bzzzzzz" : "..."
-        default:
-            break
-        }
-        return value
-    }
+    func getCry() -> String { "" }
 
     static func makeParrot(_ type: ParrotType, _ numberOfCoconuts: Int, _ voltage: Double, _ isNailed: Bool) -> Parrot {
         switch type {
         case .european:
-            return European(type, numberOfCoconuts, voltage, isNailed)
+            return European(numberOfCoconuts, voltage, isNailed)
 
         case .african:
-            return African(type, numberOfCoconuts, voltage, isNailed)
+            return African(numberOfCoconuts, voltage, isNailed)
 
         case .norwegianBlue:
-            return NorwegianBlue(type, numberOfCoconuts, voltage, isNailed)
+            return NorwegianBlue(numberOfCoconuts, voltage, isNailed)
         }
     }
 }
 
 class European: Parrot {
-    // init( Int,  Double,  Bool) {
-    //     self.init( numberOfCoconuts,  voltage,  isNailed)
-    // }
+    init(_ numberOfCoconuts: Int, _ voltage: Double, _ isNailed: Bool) {
+        super.init(.european, numberOfCoconuts, voltage, isNailed)
+    }
 
-    // override func speed() -> Double {
-    //     baseSpeed
-    // }
+    override func getCry() -> String {
+        return "Sqoork!"
+    }
+
+    override func speed() -> Double {
+        baseSpeed
+    }
 }
 
 class African: Parrot {
-    // init( Int,  Double,  Bool) {
-    //     type = .african
-    //     self.init( numberOfCoconuts,  voltage,  isNailed)
-    // }
+    init(_ numberOfCoconuts: Int, _ voltage: Double, _ isNailed: Bool) {
+        super.init(.african, numberOfCoconuts, voltage, isNailed)
+    }
 
-    // override func speed() -> Double {
-    //     max(0, baseSpeed - loadFactor * Double(numberOfCoconuts))
-    // }
+    override func getCry() -> String {
+        return "Sqaark!"
+    }
+
+    override func speed() -> Double {
+        max(0, baseSpeed - loadFactor * Double(numberOfCoconuts))
+    }
 }
 
 class NorwegianBlue: Parrot {
-    // init( Int,  Double,  Bool) {
-    //     type = .norwegianBlue
-    //     self.init( numberOfCoconuts,  voltage,  isNailed)
-    // }
+    init(_ numberOfCoconuts: Int, _ voltage: Double, _ isNailed: Bool) {
+        super.init(.norwegianBlue, numberOfCoconuts, voltage, isNailed)
+    }
 
-    // override func speed() -> Double {
-    //     isNailed ? 0 : baseSpeed( voltage)
-    // }
+    override func getCry() -> String {
+        return voltage > 0 ? "Bzzzzzz" : "..."
+    }
+
+    override func speed() -> Double {
+        isNailed ? 0 : baseSpeed(voltage: voltage)
+    }
 }
--- Tests/ParrotTests/ParrotTests.swift
+++ Tests/ParrotTests/ParrotTests.swift
@@ -3,57 +3,57 @@ import XCTest
 
 class ParrotTests: XCTestCase {
     func testSpeedOfEuropeanParrot() throws {
-        let parrot = Parrot(.european, 0, 0.0, false)
+        let parrot = Parrot.makeParrot(.european, 0, 0.0, false)
         XCTAssertEqual(parrot.speed(), 12.0)
     }
 
     func testSpeedOfAfricanParrot_with_one_coconut() throws {
-        let parrot = Parrot(.african, 1, 0.0, false)
+        let parrot = Parrot.makeParrot(.african, 1, 0.0, false)
         XCTAssertEqual(parrot.speed(), 3.0)
     }
 
     func testSpeedOfAfricanParrot_with_two_coconuts() throws {
-        let parrot = Parrot(.african, 2, 0.0, false)
+        let parrot = Parrot.makeParrot(.african, 2, 0.0, false)
         XCTAssertEqual(parrot.speed(), 0.0)
     }
 
     func testSpeedOfAfricanParrot_with_no_coconuts() throws {
-        let parrot = Parrot(.african, 0, 0.0, false)
+        let parrot = Parrot.makeParrot(.african, 0, 0.0, false)
         XCTAssertEqual(parrot.speed(), 12.0)
     }
 
     func testSpeedOfNorwegianBlueParrot_nailed() throws {
-        let parrot = Parrot(.norwegianBlue, 0, 0.0, true)
+        let parrot = Parrot.makeParrot(.norwegianBlue, 0, 0.0, true)
         XCTAssertEqual(parrot.speed(), 0.0)
     }
 
     func testSpeedOfNorwegianBlueParrot_not_nailed() throws {
-        let parrot = Parrot(.norwegianBlue, 0, 1.5, false)
+        let parrot = Parrot.makeParrot(.norwegianBlue, 0, 1.5, false)
         XCTAssertEqual(parrot.speed(), 18.0)
     }
 
     func testSpeedOfNorwegianBlueParrot_not_nailed_high_voltage() throws {
-        let parrot = Parrot(.norwegianBlue, 0, 4.0, false)
+        let parrot = Parrot.makeParrot(.norwegianBlue, 0, 4.0, false)
         XCTAssertEqual(parrot.speed(), 24.0)
     }
 
     public func testgetCryOfEuropeanParrot() throws {
-        let parrot = Parrot(.european, 0, 0, false)
+        let parrot = Parrot.makeParrot(.european, 0, 0, false)
         XCTAssertEqual("Sqoork!", parrot.getCry())
     }
 
     public func testgetCryOfAfricanParrot() throws {
-        let parrot = Parrot(.african, 2, 0, false)
+        let parrot = Parrot.makeParrot(.african, 2, 0, false)
         XCTAssertEqual("Sqaark!", parrot.getCry())
     }
 
     public func testgetCryNorwegianBlueParrot_high_voltage() throws {
-        let parrot = Parrot(.norwegianBlue, 0, 4, false)
+        let parrot = Parrot.makeParrot(.norwegianBlue, 0, 4, false)
         XCTAssertEqual("Bzzzzzz", parrot.getCry())
     }
 
     public func testgetCryNorwegianBlueParrot_no_voltage() throws {
-        let parrot = Parrot(.norwegianBlue, 0, 0, false)
+        let parrot = Parrot.makeParrot(.norwegianBlue, 0, 0, false)
         XCTAssertEqual("...", parrot.getCry())
     }
 }
